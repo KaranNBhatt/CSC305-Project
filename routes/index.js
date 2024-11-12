86f0710058d14404a55e1174e8f6d03f1d5f92a2
KNBhatt <knbhatt001@converse.edu>
2024-11-12 14:07:26 -0500

    Actual Initialization for Pug

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (req.app.locals.db) {
+    console.log('Got the database');
+  }
   res.render('index', { title: 'Express' });
 });
 
